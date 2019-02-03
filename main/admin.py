56ee0d8ae2059527a9655ca1e9bf68c460b93910
themasterking <westbook90@gmail.com>
2019-02-03 14:36:19 +0100

    RAS

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Tutoriel
 
 # Register your models here.
+
+class TutorielAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Titre/Date', {'fields': ['tutoriel_titre', 'tutoriel_publier']}),
+        ('Contenu', {'fields': ['tutoriel_contenu']})
+    ]
+
+admin.site.register(Tutoriel, TutorielAdmin)
--- main/models.py
+++ main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class Tutoriel(models.Model):
     tutoriel_titre = models.CharField(max_length=200)
     tutoriel_contenu = models.TextField()
-    tutoriel_publier = models.DateTimeField("date publiée")
+    tutoriel_publier = models.DateTimeField("date publiée", default=datetime.now())
 
     def __str__(self):
         return self.tutoriel_titre
