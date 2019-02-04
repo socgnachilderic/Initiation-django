b608ccd1d50a7f972867b100c44dbeb27fc3bf69
SOCGNA KOUYEM Childéric <westbook90@gmail.com>
2019-02-04 20:56:49 +0100

    Ajout du dossier <<static>>, fichier <<style.css>> et utilisation de modules de login de django

--- main/urls.py
+++ main/urls.py
@@ -20,4 +20,6 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_request, name='login'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+#from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from .models import *
 
 # Create your views here.
 
 def homepage(request):
     return render(request, 'main/home.html', context={"tutoriel": Tutoriel.objects.all})
+
+
+def register(request):
+    form = UserCreationForm
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('main:homepage')
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+    return render(request, 'main/register.html', context={"form": form})
+
+
+def login_request(request):
+    pass
